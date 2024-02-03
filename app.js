898d76a5ea5afd4ca33abe9317189c060541b879
Haryati H <haryati75@gmail.com>
2024-02-03 14:26:04 +0800

    add comments, fix about navigation

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+// use browser's Inspect -> Console to see the console.log output
 console.log('Hey, this will output to the browser\'s console.');
 
 // logic to hide posts according to Filter click
+// ----------------------------------------------
+// get the list of categories
 const filterList = document.querySelectorAll('#categories li');
 
+// add click event to each category (loop through the list)
 filterList.forEach(item => {
   item.addEventListener('click', (e) => {
     // get the category name
     const filterName = e.target.innerText.toLowerCase();
+    console.log('clicked', filterName);
 
     // remove the selected class from all categories
     filterList.forEach(item => {
@@ -18,6 +23,7 @@ filterList.forEach(item => {
     // hide or display the posts according to the category
     const posts = document.querySelectorAll('#posts article');
     posts.forEach(post => {
+      // check if the post has the same category as the clicked category
       if (post.getAttribute('data-category') === filterName || filterName === 'show all') {
         post.style.display = 'block';
       } else {
